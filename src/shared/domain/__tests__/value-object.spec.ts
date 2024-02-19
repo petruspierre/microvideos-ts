ca02daa061616438f4957fa56c714b33dd1dd889
Petrus Pierre <petrus.tsx@gmail.com>
2024-02-19 17:24:13 -0300

    add value objects

---
+++ src/shared/domain/__tests__/value-object.spec.ts
@@ -0,0 +1,46 @@
+import { ValueObject } from "../value-object";
+
+class StringValueObject extends ValueObject {
+  constructor(readonly value: string) {
+    super();
+  }
+}
+
+describe('ValueObject unit tests', () => {
+  describe('equals', () => {
+    it('should return true if two value objects are equal', () => {
+      const valueObject1 = new StringValueObject('test');
+      const valueObject2 = new StringValueObject('test');
+
+      expect(valueObject1.equals(valueObject2)).toBe(true);
+    });
+
+    it('should return false if two value objects are not equal', () => {
+      const valueObject1 = new StringValueObject('test');
+      const valueObject2 = new StringValueObject('test2');
+
+      expect(valueObject1.equals(valueObject2)).toBe(false);
+    });
+
+    it('should return false if the value is null', () => {
+      const valueObject1 = new StringValueObject('test');
+      const valueObject2 = null;
+
+      expect(valueObject1.equals(valueObject2!)).toBe(false);
+    });
+
+    it('should return false if the value is undefined', () => {
+      const valueObject1 = new StringValueObject('test');
+      const valueObject2 = undefined;
+
+      expect(valueObject1.equals(valueObject2)).toBe(false);
+    });
+
+    it('should return false if the value is a different type', () => {
+      const valueObject1 = new StringValueObject('test');
+      const valueObject2 = { value: 'test' };
+
+      expect(valueObject1.equals(valueObject2 as StringValueObject)).toBe(false);
+    });
+  })
+})
---
+++ src/shared/domain/value-object.ts
@@ -0,0 +1,15 @@
+import isEqual from "lodash/isEqual";
+
+export abstract class ValueObject {
+  equals(value?: this): boolean {
+    if (value === null || value === undefined) {
+      return false;
+    }
+
+    if (value.constructor.name !== this.constructor.name) {
+      return false;
+    }
+
+    return isEqual(value, this);
+  }
+}
