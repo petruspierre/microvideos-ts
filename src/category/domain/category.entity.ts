e816ec3c5f47c1666eb74bb817151a323976f742
Petrus Pierre <petrus.tsx@gmail.com>
2024-02-21 20:46:43 -0300

    create entity abstraction

--- src/category/domain/category.entity.ts
+++ src/category/domain/category.entity.ts
@@ -1,3 +1,4 @@
+import { Entity } from "../../shared/domain/entity";
 import { EntityValidationError } from "../../shared/domain/validators/validation.error";
 import { Uuid } from "../../shared/domain/value-objects/uuid.vo";
 import { CategoryValidatorFactory } from "./category.validator";
@@ -8,15 +9,15 @@ export type CategoryConstructorProps = {
   description?: string | null;
   is_active?: boolean;
   created_at?: Date;
-}
+};
 
 export type CategoryCreateCommand = {
   name: string;
   description?: string | null;
   is_active?: boolean;
-}
+};
 
-export class Category {
+export class Category extends Entity {
   category_id: Uuid;
   name: string;
   description: string | null;
@@ -24,11 +25,16 @@ export class Category {
   created_at: Date;
 
   constructor(props: CategoryConstructorProps) {
+    super();
     this.category_id = props.category_id || new Uuid();
     this.name = props.name;
     this.description = props.description ?? null;
     this.is_active = props.is_active ?? true;
     this.created_at = props.created_at ?? new Date();
+  }
+
+  get entity_id() {
+    return this.category_id;
   }
 
   static create(props: CategoryCreateCommand): Category {
@@ -60,8 +66,11 @@ export class Category {
     const validator = CategoryValidatorFactory.create();
     const isValid = validator.validate(entity);
 
-    if(!isValid) {
-      throw new EntityValidationError(validator.errors, 'Category validation error')
+    if (!isValid) {
+      throw new EntityValidationError(
+        validator.errors,
+        "Category validation error"
+      );
     }
   }
 
