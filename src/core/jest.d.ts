ca3fd1be5907bf03bffb2e0de84f51e97d0aa632
Petrus Pierre <petrus.tsx@gmail.com>
2024-02-26 18:53:26 -0300

    refactor and add nestjs

---
+++ src/core/jest.d.ts
@@ -0,0 +1,9 @@
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      notificationContainsErrorMessages: (
+        expected: Array<string | { [key: string]: string[] }>,
+      ) => R;
+    }
+  }
+}
