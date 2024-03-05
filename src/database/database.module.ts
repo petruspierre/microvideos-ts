d8e4a0432be450d9dee9ae0c88f23b1f3758feac
Petrus Pierre <petrus.tsx@gmail.com>
2024-03-04 21:11:59 -0300

    add nestjs and basic modules

---
+++ src/database/database.module.ts
@@ -0,0 +1,45 @@
+import { CategoryModel } from '@core/category/infra/db/sequelize/category.model';
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { CONFIG_SCHEMA_TYPE } from 'src/config/config.module';
+
+const models = [CategoryModel];
+
+@Module({
+  imports: [
+    SequelizeModule.forRootAsync({
+      useFactory: (configService: ConfigService<CONFIG_SCHEMA_TYPE>) => {
+        const dialect = configService.get('DB_VENDOR');
+
+        if (dialect === 'sqlite') {
+          return {
+            dialect,
+            host: configService.get('DB_HOST'),
+            models,
+            logging: configService.get('DB_LOGGING'),
+            autoLoadModels: configService.get('DB_AUTO_LOAD_MODELS'),
+          };
+        } else if (dialect === 'mysql') {
+          return {
+            dialect,
+            host: configService.get('DB_HOST'),
+            port: configService.get('DB_PORT'),
+            database: configService.get('DB_DATABASE'),
+            username: configService.get('DB_USERNAME'),
+            password: configService.get('DB_PASSWORD'),
+            models,
+            logging: configService.get('DB_LOGGING'),
+            autoLoadModels: configService.get('DB_AUTO_LOAD_MODELS'),
+          };
+        }
+
+        throw new Error(
+          `Database dialect ${dialect} is not supported. Supported dialects are: mysql, sqlite`,
+        );
+      },
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {}
