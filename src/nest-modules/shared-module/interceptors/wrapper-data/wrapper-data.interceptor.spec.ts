68bb1e1201de16c8383634ae1558f75d46fb3887
Petrus Pierre <petrus.tsx@gmail.com>
2024-03-12 17:53:34 -0300

    add wrapper data interceptor

---
+++ src/nest-modules/shared-module/interceptors/wrapper-data/wrapper-data.interceptor.spec.ts
@@ -0,0 +1,29 @@
+import { lastValueFrom, of } from 'rxjs';
+import { WrapperDataInterceptor } from './wrapper-data.interceptor';
+
+describe('WrapperDataInterceptor', () => {
+  let interceptor: WrapperDataInterceptor;
+
+  beforeEach(() => {
+    interceptor = new WrapperDataInterceptor();
+  });
+
+  it('should wrap with data key', async () => {
+    const obs$ = interceptor.intercept(null, {
+      handle: () => of({ name: test }),
+    });
+
+    const result = await lastValueFrom(obs$)
+    expect(result).toEqual({ data: { name: test } });
+  });
+
+  it('should not wrap when meta key is present', () => {
+    const obs$ = interceptor.intercept(null, {
+      handle: () => of({ meta: { name: test } }),
+    });
+
+    obs$.subscribe((result) => {
+      expect(result).toEqual({ meta: { name: test } });
+    });
+  })
+});
