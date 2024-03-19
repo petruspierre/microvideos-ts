d5c51c637b3a42ca08ffa3ad5987658fa585c4fe
Petrus Pierre <petrus.tsx@gmail.com>
2024-03-18 21:23:14 -0300

    add migrations

--- jest.config.ts
+++ jest.config.ts
@@ -9,22 +9,34 @@ const config: Config = {
   moduleFileExtensions: ['js', 'json', 'ts'],
   rootDir: 'src',
   testRegex: '.*\\..*spec\\.ts$',
-  collectCoverageFrom: ['**/*.(t|j)s'],
-  coverageDirectory: '../coverage',
-  testEnvironment: 'node',
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
-  },
-  clearMocks: true,
-  setupFilesAfterEnv: ['./core/shared/infra/testing/expect-helpers.ts'],
   transform: {
     '^.+\\.(t|j)s$': '@swc/jest',
   },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '.interface.ts',
+    '-interface.ts',
+    'shared/testing',
+    'shared-module/testing',
+    'validator-rules.ts',
+    '-fixture.ts',
+    '.input.ts',
+    '.d.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['./core/shared/infra/testing/expect-helpers.ts'],
+  coverageProvider: 'v8',
+  clearMocks: true,
 };
 
 export default config;
